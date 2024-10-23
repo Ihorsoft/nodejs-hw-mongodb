adadd9f28994cb28f0465d88321301010923bff0
Ihorsoft <0639492002s@gmail.com>
2024-10-23 11:05:47 +0300

    after env()

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
+  await initMongoConnection();
   // await initMongoDB();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
 
-  await initMongoConnection(); // old
+  // await initMongoConnection(); // old
   setupServer();
 };
-bootstrap();
-//void bootstrap();
+//bootstrap();
+void bootstrap();
